2aba3d458542b951ead17f114c2f07fb87d0d3cd
Ritik <negiritik47@gmail.com>
2024-06-10 10:49:39 +0530

    gpt integration

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,19 +1,22 @@
-// import React from 'react'
-
+import React from "react";
 import {
   useMultiChatLogic,
   MultiChatSocket,
   MultiChatWindow,
-  MessageForm,
 } from "react-chat-engine-advanced";
-import Header from "../customHeader/index.jsx";
-import StandardMessageForm from "../customMessageForms/StandardMessageForm.jsx";
-const Chat = () => {
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+import Ai from "@/components/customMessageForms/Ai";
+import AiCode from "@/components/customMessageForms/AiCode";
+import AiAssist from "@/components/customMessageForms/AiAssist";
+
+const Chat = ({ user, secret }) => {
   const chatProps = useMultiChatLogic(
-    import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
-    "testUser",
-    "1234"
+    import.meta.env.VITE_PROJECT_ID,
+    user,
+    secret
   );
+
   return (
     <div style={{ flexBasis: "100%" }}>
       <MultiChatSocket {...chatProps} />
@@ -22,6 +25,16 @@ const Chat = () => {
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
+          }
+
           return (
             <StandardMessageForm props={props} activeChat={chatProps.chat} />
           );
